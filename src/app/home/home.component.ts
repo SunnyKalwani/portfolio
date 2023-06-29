dcb1a4eb5749e7508fa1dd5a466a9b70618204d4
Sunny K <sunnydkal@gmail.com>
2023-06-28 21:24:41 -0400

    added feature to download resume on click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private renderer: Renderer2){}
+
+  ResumeDownload(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/Sunny Kalwani Resume.pdf');
+    link.setAttribute('download', `Sunny Kalwani Resume.pdf`);
+    link.click();
+    link.remove();
+  }
 
 }
